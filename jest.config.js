5287464cf37935776cc35696ae7abe7f935d3c02
Victor Freitas <victor.freitas@luizalabs.com>
2022-10-07 00:05:56 -0300

    chore: add jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,24 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  coverageDirectory: '.jest-coverage',
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  coverageReporters: ['html', 'lcov', 'text-summary', 'cobertura'],
+  moduleDirectories: ['node_modules'],
+  transformIgnorePatterns: ['/node_modules/(?!@babel).+\\.js$'],
+  moduleNameMapper: {
+    'components/(.*)$': '<rootDir>/src/components/$1'
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'babel-jest',
+      {
+        caller: {
+          supportsDynamicImport: true,
+          supportsStaticESM: true
+        },
+        configFile: './.babelrc'
+      }
+    ]
+  }
 }
